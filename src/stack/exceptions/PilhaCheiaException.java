7867ce906d80bb9e55b4943562b5510e12616853
anahenschel <anahenschel93@gmail.com>
2025-06-19 16:07:11 -0300

    Adding stack logic

---
+++ src/stack/exceptions/PilhaCheiaException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package stack.exceptions;
+
+public class PilhaCheiaException extends RuntimeException {
+
+    public PilhaCheiaException() {
+    }
+
+    /**
+     * Constructs an instance of <code>PilhaCheiaException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public PilhaCheiaException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/stack/interfaces/Pilha.java
@@ -0,0 +1,9 @@
+package stack.interfaces;
+
+public interface Pilha<T> {
+    void push(T info);
+    T pop();
+    T peek();
+    boolean estaVazia();
+    void liberar();
+}
